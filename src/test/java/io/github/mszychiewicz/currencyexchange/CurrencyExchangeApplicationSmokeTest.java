097d88aa06c47d04ee3596b1c752d3c596badc41
Mateusz Szychiewicz <szychiewiczmateusz@gmail.com>
2021-03-08 20:46:00 +0100

    spring boot init

---
+++ src/test/java/io/github/mszychiewicz/currencyexchange/CurrencyExchangeApplicationSmokeTest.java
@@ -0,0 +1,13 @@
+package io.github.mszychiewicz.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeApplicationSmokeTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
