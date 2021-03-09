b3d63536f0c710c8bf1833c2ef856b2d508440bd
Mateusz Szychiewicz <szychiewiczmateusz@gmail.com>
2021-03-09 19:56:23 +0100

    application test using wiremock rework

--- src/test/java/io/github/mszychiewicz/currencyexchange/CurrencyExchangeApplicationTest.java
+++ src/test/java/io/github/mszychiewicz/currencyexchange/CurrencyExchangeApplicationTest.java
@@ -1,12 +1,11 @@
 package io.github.mszychiewicz.currencyexchange;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.github.mszychiewicz.currencyexchange.api.request.BuyCurrencyRequest;
-import io.github.mszychiewicz.currencyexchange.api.request.OpenAccountRequest;
-import io.github.mszychiewicz.currencyexchange.api.request.SellCurrencyRequest;
-import io.github.mszychiewicz.currencyexchange.api.response.AccountResponse;
-import io.github.mszychiewicz.currencyexchange.api.response.OpenAccountResponse;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.github.mszychiewicz.currencyexchange.config.NbpMockServer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,14 +14,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
-import java.util.Currency;
-import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-// TODO mock nbp api using wiremock
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,127 +30,143 @@ class CurrencyExchangeApplicationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
-    void openAccountAndGetDetails() throws Exception {
-        //given open account request
-        OpenAccountRequest openAccountRequest = new OpenAccountRequest("Jane", "Doe", BigDecimal.TEN);
+    @RegisterExtension
+    NbpMockServer nbpMockServer = new NbpMockServer();
 
-        //when open account
+    @Test
+    void givenUserData_whenUserOpensAccount_thenAccountIsCorrect() throws Exception {
+        //given user wants to open an account
+        String firstName = "Jane";
+        String lastName = "Doe";
+        BigDecimal plnBalance = BigDecimal.TEN;
+
+        //when they try to open account expect success
+        String accountId = openAccount(firstName, lastName, plnBalance);
+
+        //and when user gets their account details expect success
+        JsonNode accountDetails = getAccountDetails(accountId);
+
+        //then user account details are correct
+        assertEquals(accountId, accountDetails.get("id").textValue());
+        assertEquals(firstName, accountDetails.get("firstName").textValue());
+        assertEquals(lastName, accountDetails.get("lastName").textValue());
+        //and account balance is correct
+        BigDecimal plnAccountBalance = new BigDecimal(accountDetails.path("balances").get("PLN").toString());
+        assertEquals(plnBalance, plnAccountBalance);
+    }
+
+    @Test
+    void givenUserAccountWithSufficientBalance_whenUserBuysCurrency_thenExchangeIsCorrect() throws Exception {
+        //given user has open account with sufficient balance
+        String firstName = "Jane";
+        String lastName = "Doe";
+        BigDecimal plnBalance = BigDecimal.TEN;
+        String accountId = openAccount(firstName, lastName, plnBalance);
+
+        //and wants to buy an amount of USD currency
+        String currencyCode = "USD";
+        BigDecimal amountToBuy = BigDecimal.ONE;
+
+        //and there are exchange rates
+        BigDecimal askExchangeRate = new BigDecimal("3.9112");
+        nbpMockServer.stubExchangeRateResponse(currencyCode, "3.8421", askExchangeRate.toString());
+
+        //when they try to buy currency expect success
+        buyCurrency(accountId, currencyCode, amountToBuy);
+
+        //and when they try to get their account details expect success
+        JsonNode accountDetails = getAccountDetails(accountId);
+
+        //then currency balance is equal to bought amount
+        BigDecimal currencyAccountBalance = new BigDecimal(accountDetails.path("balances").get(currencyCode).toString());
+        assertEquals(amountToBuy, currencyAccountBalance);
+
+        //and PLN balance is reduced by correct exchanged amount
+        BigDecimal plnAccountBalance = new BigDecimal(accountDetails.path("balances").get("PLN").toString());
+        assertEquals(plnBalance.subtract(amountToBuy.multiply(askExchangeRate)), plnAccountBalance);
+    }
+
+    @Test
+    void givenUserAccountWithSufficientBalance_whenUserSellsCurrency_thenExchangeIsCorrect() throws Exception {
+        //given user has open account
+        String firstName = "Jane";
+        String lastName = "Doe";
+        BigDecimal plnBalance = BigDecimal.TEN;
+        String accountId = openAccount(firstName, lastName, plnBalance);
+
+        //and wants to sell an amount of USD currency
+        String currencyCode = "USD";
+        BigDecimal amountToSell = BigDecimal.ONE;
+
+        //and there are exchange rates
+        BigDecimal bidExchangeRate = new BigDecimal("3.8421");
+        BigDecimal askExchangeRate = new BigDecimal("3.9112");
+        nbpMockServer.stubExchangeRateResponse(currencyCode, bidExchangeRate.toString(), askExchangeRate.toString());
+
+        //and has sufficient balance
+        BigDecimal currencyBalance = BigDecimal.ONE;
+        buyCurrency(accountId, currencyCode, currencyBalance);
+        plnBalance = plnBalance.subtract(currencyBalance.multiply(askExchangeRate));
+
+        //when they try to sell currency expect success
+        sellCurrency(accountId, currencyCode, amountToSell);
+
+        //and when they try to get their account details expect success
+        JsonNode accountDetails = getAccountDetails(accountId);
+
+        //then currency balance is reduced by correct amount
+        BigDecimal currencyAccountBalance = new BigDecimal(accountDetails.path("balances").get(currencyCode).toString());
+        assertEquals(currencyBalance.subtract(amountToSell), currencyAccountBalance);
+
+        //and PLN balance is increased by correct exchanged amount
+        BigDecimal plnAccountBalance = new BigDecimal(accountDetails.path("balances").get("PLN").toString());
+        assertEquals(plnBalance.add(amountToSell.multiply(bidExchangeRate)), plnAccountBalance);
+    }
+
+    private String openAccount(String firstName, String lastName, BigDecimal plnBalance) throws Exception {
+        ObjectNode openAccountData = objectMapper.createObjectNode();
+        openAccountData.put("firstName", firstName);
+        openAccountData.put("lastName", lastName);
+        openAccountData.put("openingBalance", plnBalance.toString());
+
         MvcResult openAccountResult = mvc.perform(post("/account")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(openAccountRequest)))
+                .content(objectMapper.writeValueAsString(openAccountData)))
                 .andExpect(status().isOk())
                 .andReturn();
 
-        //then account id is returned
         String openAccountContent = openAccountResult.getResponse().getContentAsString();
-        UUID accountId = objectMapper.readValue(openAccountContent, OpenAccountResponse.class).getId();
+        return objectMapper.readTree(openAccountContent).get("id").textValue();
+    }
 
-        //when get account details
+    private JsonNode getAccountDetails(String accountId) throws Exception {
         MvcResult getAccountDetailsResult = mvc.perform(get("/account/" + accountId))
                 .andExpect(status().isOk())
                 .andReturn();
 
-        //then account details match request
         String getAccountDetailsContent = getAccountDetailsResult.getResponse().getContentAsString();
-        AccountResponse response = objectMapper.readValue(getAccountDetailsContent, AccountResponse.class);
-
-        assert (response.getFirstName().equals(openAccountRequest.getFirstName()));
-        assert (response.getLastName().equals(openAccountRequest.getSecondName()));
-        assert (response.getBalances().get(
-                Currency.getInstance("PLN")).equals(openAccountRequest.getOpeningBalance())
-        );
+        return objectMapper.readTree(getAccountDetailsContent);
     }
 
-    @Test
-    void openAccountAndBuyUsd() throws Exception {
-        //given open account request and buy currency request
-        OpenAccountRequest openAccountRequest = new OpenAccountRequest("Jane", "Doe", BigDecimal.TEN);
-        BuyCurrencyRequest buyCurrencyRequest = new BuyCurrencyRequest("USD", BigDecimal.ONE);
+    private void buyCurrency(String accountId, String currencyCode, BigDecimal amount) throws Exception {
+        ObjectNode buyCurrencyData = objectMapper.createObjectNode();
+        buyCurrencyData.put("currencyCode", currencyCode);
+        buyCurrencyData.put("amount", amount.toString());
 
-        //when open account
-        MvcResult openAccountResult = mvc.perform(post("/account")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(openAccountRequest)))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        //then account id is returned
-        String openAccountContent = openAccountResult.getResponse().getContentAsString();
-        UUID accountId = objectMapper.readValue(openAccountContent, OpenAccountResponse.class).getId();
-
-        //when buy usd
         mvc.perform(post("/account/" + accountId + "/buy-currency-command")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(buyCurrencyRequest)))
+                .content(objectMapper.writeValueAsString(buyCurrencyData)))
                 .andExpect(status().isOk());
-
-        //and when get account details
-        MvcResult getAccountDetailsResult = mvc.perform(get("/account/" + accountId))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        //then account pln balance is lower than opening balance and currency balance matches buy request
-        String getAccountDetailsContent = getAccountDetailsResult.getResponse().getContentAsString();
-        AccountResponse response = objectMapper.readValue(getAccountDetailsContent, AccountResponse.class);
-
-        assert (response.getFirstName().equals(openAccountRequest.getFirstName()));
-        assert (response.getLastName().equals(openAccountRequest.getSecondName()));
-        assert (response.getBalances().get(
-                Currency.getInstance("PLN")).compareTo(openAccountRequest.getOpeningBalance()) < 0
-        );
-        assert (response.getBalances().get(
-                Currency.getInstance(buyCurrencyRequest.getCurrencyCode())).equals(buyCurrencyRequest.getAmount())
-        );
     }
 
-    @Test
-    void openAccountAndBuyThenSellUsd() throws Exception {
-        //given open account request and buy currency request
-        OpenAccountRequest openAccountRequest = new OpenAccountRequest("Jane", "Doe", BigDecimal.TEN);
-        BuyCurrencyRequest buyCurrencyRequest = new BuyCurrencyRequest("USD", BigDecimal.ONE);
-        SellCurrencyRequest sellCurrencyRequest = new SellCurrencyRequest("USD", BigDecimal.ONE);
+    private void sellCurrency(String accountId, String currencyCode, BigDecimal amount) throws Exception {
+        ObjectNode sellCurrencyData = objectMapper.createObjectNode();
+        sellCurrencyData.put("currencyCode", currencyCode);
+        sellCurrencyData.put("amount", amount.toString());
 
-        //when open account
-        MvcResult openAccountResult = mvc.perform(post("/account")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(openAccountRequest)))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        //then account id is returned
-        String openAccountContent = openAccountResult.getResponse().getContentAsString();
-        UUID accountId = objectMapper.readValue(openAccountContent, OpenAccountResponse.class).getId();
-
-        //when buy usd
-        mvc.perform(post("/account/" + accountId + "/buy-currency-command")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(buyCurrencyRequest)))
-                .andExpect(status().isOk());
-
-        //and when next sell usd
         mvc.perform(post("/account/" + accountId + "/sell-currency-command")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(sellCurrencyRequest)))
+                .content(objectMapper.writeValueAsString(sellCurrencyData)))
                 .andExpect(status().isOk());
-
-        //and when get account details
-        MvcResult getAccountDetailsResult = mvc.perform(get("/account/" + accountId))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        //then account pln balance is lower than opening balance due to ask and bid spread
-        //and usd balance is 0
-        String getAccountDetailsContent = getAccountDetailsResult.getResponse().getContentAsString();
-        AccountResponse response = objectMapper.readValue(getAccountDetailsContent, AccountResponse.class);
-
-        assert (response.getFirstName().equals(openAccountRequest.getFirstName()));
-        assert (response.getLastName().equals(openAccountRequest.getSecondName()));
-        assert (response.getBalances().get(
-                Currency.getInstance("PLN")).compareTo(openAccountRequest.getOpeningBalance()) < 0
-        );
-        assert (response.getBalances().get(
-                Currency.getInstance(buyCurrencyRequest.getCurrencyCode())).equals(BigDecimal.ZERO)
-        );
     }
 }
