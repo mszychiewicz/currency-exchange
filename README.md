db1c79b58edded2e6704f2dd7e2b7df2397eb095
Mateusz Szychiewicz <szychiewiczmateusz@gmail.com>
2021-03-10 13:41:42 +0100

    added swagger

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# currency-exchange
+
+<div align=center>
+  <h1>Currency Exchange</h1>
+  <p>Currency exchange platform.</p>
+</div>
+
+## Start:
+
+    ./gradlew bootRun
+
+## Api documentation:
+
+    http://localhost:8080/swagger-ui.html
--- src/main/java/io/github/mszychiewicz/currencyexchange/Config.java
+++ src/main/java/io/github/mszychiewicz/currencyexchange/Config.java
@@ -4,6 +4,10 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.time.Duration;
 
@@ -18,4 +22,12 @@ public class Config {
                 .build();
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
